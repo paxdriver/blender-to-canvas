7ac6dd129f008669552cfb5eab37d04c7a314eba
Kristopher Driver <paxdriver@gmail.com>
2024-11-30 16:04:16 -0600

    WIP - working on manual wireframe rotation to make sure the mesh is rendering as expected. No output, methinks I got the trig functions confused and scaleFactor method. Just a progress update so i can back-track if i need to.

--- canvas-script.js
+++ canvas-script.js
@@ -8,7 +8,9 @@ async function get_model_data(){
 const BUFFER_SIZE = 4
 const [WIDTH, HEIGHT, SIZE] = [800, 800, 100]
 const CAMERA_POSITION = [0, 0, 10]
-const BASIC_MINIMUM = 0.00001
+const BASIC_MINIMUM = 0.00000001
+const INTEGER_CONVERSION_SCALE_FACTOR = 1
+const DOT_SIZE = 20
 // ---------------------------------------------------------
 
 // initialize the canvas and page
@@ -30,11 +32,12 @@ function drawLineFromTo(a, b){ // a and b are arrays [x,y,z]
     ctx.lineCap = "round"
     ctx.stroke()
 }
-function drawVerts(x, y, z){
-    let _dot_size = (z*SIZE) / 20
-    if (_dot_size < 0) _dot_size = _dot_size*-1
+function drawVerts(x, y, scaleFactor){
+    console.log(x, y, scaleFactor) // getting 0, 0, undefined!!! I'VE MADE AN ERROR SOMEWHERE WHEN THIS IS CALLED
+    let _dot_size = DOT_SIZE * scaleFactor
+    if (_dot_size < 2) _dot_size = 2
     ctx.beginPath()
-    ctx.arc(x*SIZE, y*SIZE, _dot_size, 0, 2*Math.PI)
+    ctx.arc(x*SIZE, y*SIZE, _dot_size, 0, 2 * Math.PI)
     ctx.fillStyle = 'green'
     ctx.fill()
     ctx.stroke()
@@ -50,88 +53,145 @@ function drawVerts(x, y, z){
     Since Vert extends BufferedData, apply transformation methods in BufferedData rather than Vert class to ensure the functions are not being needlessly copied with each instantiation of vertices.
 */
 
+// Methods to be included with every Vert class object
 class BufferedData{
     constructor(bufferLength) {
         this.buffer = new ArrayBuffer(bufferLength)
         this.view = new Float32Array(this.buffer)
     }
     // handle values to avoid 0 values
-    checkForZero() { 
+    checkForZeros() { 
         for (let i = 0; i < this.view.length; i++) {
-            this.view[i] = (this.view[i] < BASIC_MINIMUM && this.view[i] !== 0) ? BASIC_MINIMUM : this.view[i]
+            if (this.view[i] < BASIC_MINIMUM && this.view[i] !== 0) {
+                this.view[i] = BASIC_MINIMUM 
+            } 
         }
     }
-    // Compute X coordinate
-    getComputedX(){ return this.view[0] / this.view[2] }
-
-    // Compute Y coordinate
-    getComputedY(){ return this.view[1] / this.view[2] }
-
-    // get computed value of X and Y computed
-    updateCoordinates(){
-        this.checkForZero()
-        this.view[0] = this.getComputedX()
-        this.view[1] = this.getComputedY()
-    }
-}
-
-class Vert extends BufferedData {
-    constructor(x, y, z){
-        super(BUFFER_SIZE * arguments.length)
-        this.setView(x,y,z)
-    }
-
-    setView(x, y, z){
+    setView(x, y, z){ // Float values of coordinates from imported data
         this.view[0] = x
         this.view[1] = y
         this.view[2] = z
     }
-    getX(){ return this.view[0] }
-    getY(){ return this.view[1] }
-    getZ(){ return this.view[2] }
-}
+    // returns canvas-friendly integer value
+    getX(scaleFactor){
+        let output = Math.round(this.view[0] * INTEGER_CONVERSION_SCALE_FACTOR * scaleFactor)
+        this.computed_view[0] = output
+    }
+    getY(scaleFactor){ 
+        this.computed_view[1] = Math.round(this.view[1] * INTEGER_CONVERSION_SCALE_FACTOR * scaleFactor)
+    }
+    updateCoordsWithDepthFactor(scaleFactor){
+        this.getX(scaleFactor)
+        this.getY(scaleFactor)
+    }
+    // WORK IN PROGRESS!!! TRYING MANUAL ROTATION TO MAKE SURE POINTS AND EDGES ARE BEING PLOTTED AS EXPECTED. HARD TO TELL FROM FACE-ON VIEW
+    rotateY(amt){
+        let x = this.view[2] / this.view[0]
+        x *= Math.cos(amt) + this.view[0]
+        this.view[0] = x
+        this.computed_view[0] = Math.round(x)
 
-class Edge {
-    constructor(v1, v2){
-        this.a = v1
-        this.b = v2
+        let z = this.view[0] / this.view[2]
+        z *= -Math.sin(amt) + this.view[0]
+        this.view[2]
+        this.computed_view[2] = Math.round(z)
+    }
+
+}
+// VERT CLASS - inherits array buffer of typed arrays, compute functions, and update functions
+class Vert extends BufferedData {
+    constructor(x, y, z){
+        super(BUFFER_SIZE * 3)
+        this.setView(x,y,z)
+        this.computed_buffer = new ArrayBuffer(2 * Int32Array.BYTES_PER_ELEMENT)
+        this.computed_view = new Int32Array(this.computed_buffer) // [x, y] integer coordinates
     }
 }
+class Edge {
+    constructor(v1, v2){
+        this.a = v1 // Vert's index
+        this.b = v2 // Vert's index
+    }
+}
+
+// Generate typed array object data for each vertex
+function map_verts(vert_data){
+    const output = []
+    vert_data.forEach( v => {
+        const vert = new Vert(v.x, v.y, v.z)
+        output.push(vert)
+    })
+    return output
+} // Generate edges pased on all Vert class objects
+function map_edges(edge_data, all_vertices){
+    const output = []
+    edge_data.forEach( v => {
+        const edge = new Edge(all_vertices[v.v1],all_vertices[v.v2])
+        output.push(edge)
+    })
+    return output
+}
+
+// Once all Vert objects are created, find the depth ranges and store a scaler in each object to be used to recalculate the x, y coordinates in 2D space with a scaler for depth adjustments
+function calculate_scale_factor(all_vertices){  // WORK IN PROGRESS!!!!
+    const minmaxZ = { 
+        min: Infinity,
+        max: -Infinity
+    }
+
+    all_vertices.forEach( vert => {
+        vert.checkForZeros()
+        if (vert.view[2] < minmaxZ.min) minmaxZ.min = vert.view[2]
+        if (vert.view[2] > minmaxZ.max) minmaxZ.max = vert.view[2]
+    })
+    all_vertices.forEach( vert => {
+        if (minmaxZ.min == minmaxZ.max) {
+            vert.updateCoordsWithDepthFactor(1)
+        } else {
+            const zRange = minmaxZ.max - minmaxZ.min
+            const zNormalized = (vert.view[2] - minmaxZ.min) / zRange     // normalize z to 0 - 1
+            const scaleFactor = 1 / (1 + zNormalized)
+            console.log(scaleFactor)
+            vert.updateCoordsWithDepthFactor(scaleFactor)
+        }
+    })
+}
+
 
 async function main(){
     const data = await get_model_data()
     const {vertices, edges} = data
+    // Map data as float32 array buffer values in Vert class objects
+    const all_vertices = map_verts(vertices) // array of all Vert objects
+    // Get depth based on min/max depth of all Vert objects and assign integer values to array buffer of integers in Vert.computed_view[0] and Vert.computed_view[1]
+    calculate_scale_factor(all_vertices)
 
-    function map_verts(vert_data){
-        const output = []
-        vert_data.forEach( v => {
-            const vert = new Vert(v.x, v.y, v.z)
-            output.push(vert)
-        })
-        return output
-    }
-    const all_vertices = map_verts(vertices)
-    console.log(all_vertices)
 
-    function map_edges(edge_data, all_vertices){
-        const output = []
-        edge_data.forEach( v => {
-            all_vertices[v.v1].updateCoordinates()
-            all_vertices[v.v2].updateCoordinates()
-            const edge = new Edge(all_vertices[v.v1],all_vertices[v.v2])
-            output.push(edge)
-        })
-        return output
-    }
-    const all_edges = map_edges(edges, all_vertices)
-    
+    //                  WORK IN PROGRESS
+    // this will be for applying rotations, translations, etc later...
+    const all_edges = map_edges(edges, all_vertices) // array of all edges connecting 2 Vert objects
+
+    // find range of depth to scale coordinates (smaller change for further away)
     function render(){
-        all_edges.forEach( v => {
-            drawLineFromTo(v.a.view, v.b.view)
+
+        all_vertices.forEach( vertex => {
+            // apply test rotation of 1/4 PI to update computed points and view points
+            vertex.rotateY(0.25 * Math.PI)
+        })
+        calculate_scale_factor(all_vertices) // recalculate the scaleFactor and store it in each instance's scaleFactor
+        all_vertices.forEach( vertex => {
+            // update x,y,z coordinates with new values from the rotation
+            vertex.updateCoordsWithDepthFactor(vertex.scaleFactor)
         })
 
+        // edges are all already just references to the Vert objects, so those updates will be live and good still
+        all_edges.forEach( vert_pair => {
+            drawLineFromTo(vert_pair.a.computed_view, vert_pair.b.computed_view)
+        })
+
+        // draw dots, where background dots are smaller than foreground ones based on the new z value after the rotation 
         all_vertices.forEach( v => {
-            drawVerts(v.view[0], v.view[1], v.view[2])
+            drawVerts(v.computed_view[0], v.computed_view[1], v.scaleFactor)
         })
 
         // requestAnimationFrame(render)
