fc5a01275fd8e6a3e7f89824edd47c6aa24094e8
Kristopher Driver <paxdriver@gmail.com>
2024-12-03 13:50:59 -0600

    Completely revised minmaxZ so that range of Z values is only computed once, then refactored class methods to eliminate redunant loops. Fixed depth scaling for dots and perspective and encapsulated entire script in IIFE to scope the pseudo-globals for when this script is eventually used in another project so that it doesn't pollute namespace (IIFE instead of block scope so that DOM elements can be passed into it to attach the canvas to later, or for passing config object or something later depending on implementation). Animation and depth now work properly. Added animation switch at top of script. Cleaned documentation. WIP: still looking for more optimizations, progress commit but working and looking good. Next will be applying faces, calculating normals for shading, then looking into 3D matrix transformations (multiple rotations at the same time).

--- canvas-script.js
+++ canvas-script.js
@@ -1,18 +1,26 @@
+(async function() { // wrapped in IIFE so that this script can be imported and called by another project with configurations provided by the caller, or a dom element can be provided to attach the canvas and animation to a container div or something...
+
 // Gather data stored as json from python script that was run in Blender to extract model data
 async function get_model_data(){
     const result = await fetch('./firstcube.json').then(response => response.json())
     return result
 }
 
+// DEV ONLY -------------------------  ANIMATION ON/OFF TOGGLE
+const animate = 1
+// DEV ONLY -------------------------  ANIMATION ON/OFF TOGGLE
+
 // GLOBAL VARIABLES TO TWEAK AND ALTER DEPENDING ON USE CASE
+// const CAMERA_POSITION = [0, 0, 0]
 const BUFFER_SIZE = 4
-const [WIDTH, HEIGHT, SIZE] = [1000, 1250, 100]
-const CAMERA_POSITION = [0, 0, 0]
-const BASIC_MINIMUM = 0.0000000001
-const INTEGER_CONVERSION_SCALE_FACTOR = 1.5
-const DEPTH_SCALING_CONSTANT = 0.25
-const DOT_SIZE = 50
-const MIN_DOT_SIZE = 2
+const [WIDTH, HEIGHT, SIZE] = [500, 500, 2]
+const BASIC_MINIMUM = 0.0000000000000001
+const INTEGER_CONVERSION_SCALE_FACTOR = 10
+const DEPTH_SCALING_CONSTANT = 1.75
+const DOT_SIZE = 5
+const MIN_DOT_SIZE = 1
+// ---------------------------------------------------------
+let minmaxZ = {min: 1, max: 1}  // initialized value, will be updated when Vert class objects get created.
 // ---------------------------------------------------------
 
 // initialize the canvas and page
@@ -29,18 +37,16 @@ ctx.translate(WIDTH/2, HEIGHT/2)
 // Rendering datapoints to canvas from edges and verts
 function drawLineFromTo(a, b){ // a and b are arrays [x,y,z]
     ctx.beginPath()
-    ctx.moveTo(a[0]*SIZE,a[1]*SIZE)
+    ctx.moveTo(a[0]*SIZE, a[1]*SIZE)
     ctx.lineTo(b[0]*SIZE, b[1]*SIZE)
     ctx.lineCap = "round"
     ctx.stroke()
 }
 function drawVerts(x, y, scaleFactor){
-    let _dot_size = DOT_SIZE * (scaleFactor*DEPTH_SCALING_CONSTANT)
+    let _dot_size = DOT_SIZE * (scaleFactor - DEPTH_SCALING_CONSTANT)
     if (_dot_size < MIN_DOT_SIZE) _dot_size = MIN_DOT_SIZE
     ctx.beginPath()
     ctx.arc(x*SIZE, y*SIZE, _dot_size, 0, 2 * Math.PI)
-    ctx.fillStyle = 'green'
-    // ctx.fill()
     ctx.stroke()
 }
 
@@ -54,7 +60,19 @@ function drawVerts(x, y, scaleFactor){
     Since Vert extends BufferedData, apply transformation methods in BufferedData rather than Vert class to ensure the functions are not being needlessly copied with each instantiation of vertices.
 */
 
-// Methods to be included with every Vert class object
+// When z coordinates change from a rotation, the range of z values across all points changes. This function calculates the new scaleFactor based on the new z coordinate after a rotation changes the z coordinate
+function setScaleFactor(z){
+    let scaleFactor = BASIC_MINIMUM
+    if (minmaxZ.min !== minmaxZ.max) { // prevent divide by zero
+        const zRange = minmaxZ.max - minmaxZ.min
+        const zNormalized = (z - minmaxZ.min !== 0) ? (z - minmaxZ.min) / zRange : 1 / BASIC_MINIMUM
+        scaleFactor = 1 / (1 + zNormalized)
+        scaleFactor += DEPTH_SCALING_CONSTANT
+    }
+    return scaleFactor
+}
+
+// Methods to be included with every Vert class object can be inherited from here to save memory and latency
 class BufferedData{
     constructor(bufferLength) {
         this.buffer = new ArrayBuffer(bufferLength)
@@ -63,62 +81,53 @@ class BufferedData{
         this._tmp = new Float32Array(this._tmpBuffer)
         this._rotationsBuffer = new ArrayBuffer(bufferLength)
         this.rotations = new Float32Array(this._rotationsBuffer)
-    }
-    // handle values to avoid 0 values
-    checkForZeros() { 
-        for (let i = 0; i < this.view.length; i++) {
-            if (this.view[i] < BASIC_MINIMUM && this.view[i] !== 0) {
-                this.view[i] = BASIC_MINIMUM 
-            } 
-        }
-    }
+    } // Setter for when Vert class object is initialized
     setView(x, y, z){ // Float values of coordinates from imported data
         this.view[0] = x
         this.view[1] = y
         this.view[2] = z
-        this.rotations = [0.0, 0.01, 0.0]
+        this.rotations = [0.0, 0.02, 0.0]
+        this.scaleFactor = setScaleFactor(z)
     }
-    // returns canvas-friendly integer value
+    // returns HTML canvas-friendly integer value
     getX(scaleFactor){
         let xValue = this._tmp[0] || this.view[0]
         this.computed_view[0] = Math.round(xValue * INTEGER_CONVERSION_SCALE_FACTOR * scaleFactor)
     }
+    // returns HTML canvas-friendly integer value
     getY(scaleFactor){
         let yValue = this._tmp[1] || this.view[1]
         this.computed_view[1] = Math.round(yValue * INTEGER_CONVERSION_SCALE_FACTOR * scaleFactor)
     }
-    getZ(){ // //DEV NOTE: don't need this right now
-        // let zValue = this._tmp[2] || this.view[2]
-    }
+    // DEV NOTE: don't need getZ() right now
+    // getZ(){ return }
+    
     updateCoordsWithDepthFactor(){
         this.getX(this.scaleFactor)
         this.getY(this.scaleFactor)
+        // this.getZ()
     }
-
-    rotateX(angle){
-        console.log(`X: ${angle}`)
-        let yValue = this._tmp[1] || this.view[1]
-        let zValue = this._tmp[2] || this.view[2]
-        this._tmp[1] = (yValue * Math.cos(angle)) + (zValue * Math.sin(angle))
-        this._tmp[2] = (zValue * Math.cos(angle)) - (yValue * Math.sin(angle))
+    rotateX(angle){ // produces new normalized coordinates
+        let yValue = this._tmp[1] || this.view[1]   // checks if there are last computed values to use
+        let zValue = this._tmp[2] || this.view[2]   // checks if there are last computed values to use
+        this._tmp[1] = (yValue * Math.cos(angle)) + (zValue * Math.sin(angle)) // assigns computed coordinates
+        this._tmp[2] = (zValue * Math.cos(angle)) - (yValue * Math.sin(angle)) // assigns computed coordinates
+        this.scaleFactor = setScaleFactor(this._tmp[2]) // as z changes, each Vert needs new scaleFactor computation
     }
     rotateY(angle){
-        console.log(`Y: ${angle}`)
         let xValue = this._tmp[0] || this.view[0]
         let zValue = this._tmp[2] || this.view[2]
         this._tmp[0] = (xValue * Math.cos(angle)) + (zValue * Math.sin(angle))
         this._tmp[2] = (zValue * Math.cos(angle)) - (xValue * Math.sin(angle))
-        console.log(`X: ${this._tmp[0]}`)
-        console.log(`Z: ${this._tmp[2]}`)
+        this.scaleFactor = setScaleFactor(this._tmp[2])
     }
     rotateZ(angle){
-        console.log(`Z: ${angle}`)
         let xValue = this._tmp[0] || this.view[0]
         let yValue = this._tmp[1] || this.view[1]
         this._tmp[0] = (xValue * Math.cos(angle)) - (yValue * Math.sin(angle))
         this._tmp[1] = (xValue * Math.sin(angle)) + (yValue * Math.cos(angle))
     }
-    applyRotations(){
+    applyRotations(){ // apply all rotations, but only call the methods with rotations that need to be applied (ie: non-zero)
         if (this.rotations[0] !== 0) this.rotateX(this.rotations[0])
         if (this.rotations[1] !== 0) this.rotateY(this.rotations[1])
         if (this.rotations[2] !== 0) this.rotateZ(this.rotations[2])
@@ -129,27 +138,30 @@ class Vert extends BufferedData {
     constructor(x, y, z){
         super(BUFFER_SIZE * 3)
         this.setView(x,y,z)
+        this.scaleFactor = (() => setScaleFactor(z))
         this.computed_buffer = new ArrayBuffer(2 * Int32Array.BYTES_PER_ELEMENT)
         this.computed_view = new Int32Array(this.computed_buffer) // [x, y] integer coordinates
     }
 }
 class Edge {
+    // Receives the index for the particular Vert object it connecting this edge. These are index values of the Vert, not the Vert's coordinates
     constructor(v1, v2){
-        this.a = v1 // Vert's index
-        this.b = v2 // Vert's index
+        this.a = v1
+        this.b = v2
     }
 }
 
-// Generate typed array object data for each vertex
-function map_verts(vert_data){
+// Generate Vert class object data for each vertex included in the imported json
+function init_map_verts(vert_data){
+    minmaxZ = calculate_scale_factor(vert_data)
     const output = []
     vert_data.forEach( v => {
         const vert = new Vert(v.x, v.y, v.z)
         output.push(vert)
     })
     return output
-} // Generate edges pased on all Vert class objects
-function map_edges(edge_data, all_vertices){
+} // Generate Edge class object data passed to all Vert class objects
+function init_map_edges(edge_data, all_vertices){
     const output = []
     edge_data.forEach( v => {
         const edge = new Edge(all_vertices[v.v1],all_vertices[v.v2])
@@ -159,28 +171,16 @@ function map_edges(edge_data, all_vertices){
 }
 
 // Once all Vert objects are created, find the depth ranges and store a scaler in each object to be used to recalculate the x, y coordinates in 2D space with a scaler for depth adjustments
-function calculate_scale_factor(all_vertices){  // WORK IN PROGRESS!!!!
-    const minmaxZ = { 
-        min: Infinity,
-        max: -Infinity
-    }
+function calculate_scale_factor(vertices){
+    vertices.forEach( vert => {
+        if (vert.z < minmaxZ.min) minmaxZ.min = vert.z
+        if (vert.z > minmaxZ.max) minmaxZ.max = vert.z
+    })
+    return minmaxZ
+}   
 
-    all_vertices.forEach( vert => {
-        if (vert.view[2] < minmaxZ.min) minmaxZ.min = vert.view[2]
-        if (vert.view[2] > minmaxZ.max) minmaxZ.max = vert.view[2]
-    })
-    all_vertices.forEach( vert => {
-        let scaleFactor = 1
-        if (minmaxZ.min == minmaxZ.max) {
-            vert.scaleFactor = scaleFactor
-        } else {
-            const zRange = minmaxZ.max - minmaxZ.min
-            const zNormalized = (vert.view[2] - minmaxZ.min) / zRange     // normalize z to 0 - 1
-            let scaleFactor = 1 / (1 + zNormalized)
-            scaleFactor += DEPTH_SCALING_CONSTANT
-            vert.scaleFactor = scaleFactor
-        }
-    })
+function assign_updated_scale_factor(all_vertices){
+    all_vertices.forEach( v => v.scaleFactor = setScaleFactor(v._tmp[2]) )
 }
 
 // ENTRY POINT
@@ -189,24 +189,26 @@ async function main(){
     const {vertices, edges} = data
     
     // Map data as float32 array buffer values in Vert class objects
-    const all_vertices = map_verts(vertices) // array of all Vert objects
+    const all_vertices = init_map_verts(vertices) // array of all Vert objects
     
-    // Get depth based on min/max depth of all Vert objects, then store the scale factor based on the range of all z values, but specific to each Vert and assign that value to the Vert objects one-by-one
-    calculate_scale_factor(all_vertices)
-
     // this will be for applying rotations, translations, etc later...
-    const all_edges = map_edges(edges, all_vertices) // array of all edges connecting 2 Vert objects
+    const all_edges = init_map_edges(edges, all_vertices) // array of all edges connecting 2 Vert objects
 
     // find range of depth to scale coordinates (smaller change for further away)
     function render(){
         ctx.clearRect(-WIDTH, -HEIGHT, WIDTH*2, HEIGHT*2)
 
-        all_vertices.forEach( vertex => {
-            vertex.applyRotations()
+        all_vertices.forEach( (vertex, idx, arr) => {
+            vertex.applyRotations() // Gets new coordinates from radians rotation held in this.rotations[x,y,z]
+            if (idx == arr.length-1) { // check if the current iteration is the last element so that global minmaxZ can be updated only once instead of on every iteration
+                // Check if rotations are applied to X or Y axes which will affect the z-range so that we only recalculate the minmaxZ when we have to
+                if (vertex.rotations[0] !== 0 || vertex.rotations[1] !==0) {
+                    minmaxZ = calculate_scale_factor(arr)
+                    assign_updated_scale_factor(arr) // apply new scaleFactor for new z coordinates after all of the rotations are done and applied, with new computed values already stored and ready to render
+                }
+            }
         })
-
-        calculate_scale_factor(all_vertices) // recalculate the scaleFactor and store it in each instance's scaleFactor
-
+        
         all_vertices.forEach( vertex => {
             // update x,y,z coordinates with new values from the rotation
             vertex.updateCoordsWithDepthFactor()
@@ -222,11 +224,11 @@ async function main(){
             drawVerts(v.computed_view[0], v.computed_view[1], v.scaleFactor)
         })
 
-        // console.log(all_vertices[all_vertices.length-1])
-        requestAnimationFrame(render)
+        if (animate) requestAnimationFrame(render)
     }
 
     render()
 }
 
 main()
+})()
